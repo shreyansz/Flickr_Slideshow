a94daddd3f927a4ffc905b09bf27ec780031d859
Shreyans Pagariya <shreyanspagariya@yahoo.com>
2014-10-24 19:49:20 -0700

    Slideshow feature

--- app.js
+++ app.js
@@ -1,18 +1,50 @@
 var main = function()
 {
-"use strict";
-
-var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=dogs&format=json&jsoncallback=?";
-
-$.getJSON(url, function (fl){
-fl.items.forEach(function(ph) 
-{
-var $i = $("<img>").hide();
-$i.attr("src",ph.media.m);
-$("main .photos").append($i);
-$("main .photos").append("<br>");
-$i.fadeIn();
-});
-});
+	"use strict";
+	$("button").on('click', function(event) 
+	{
+		displayImages($("input[name='tag']").val());	
+	});
+	$("input[name='tag']").on('keypress', function(event) 
+	{
+		if (event.keyCode === 13) 
+        {
+		displayImages($("input[name='tag']").val());	
+		}
+	});
+	var timer;
+	var displayImages = function(tags, index)
+	{
+		if(! (tags = $.trim(tags) ) )
+		{
+			console.log('err');
+			$("input[name='tag']").val('');
+			return;
+		}
+		if(!index)
+		{
+			clearTimeout(timer);
+		}
+		var url = "http://api.flickr.com/services/feeds/photos_public.gne?tags=" + tags + "&format=json&jsoncallback=?";
+		$("main .photos").empty();
+		$.getJSON(url, function (fl)
+		{
+			var picArr = fl.items;
+			if ( !index || picArr.length == index)
+			{
+				index = 0;
+			}
+			$("main .photos").empty();
+			$("main .photos").css({ 'padding-top':'5%','text-align':'center','width':'100%','height':'100%'});
+			var $i = $("<img>").width('50%').height('70%').hide();
+			$i.attr("src",picArr[index].media.m);
+			$("main .photos").append($i);
+			console.log("Showing " + index + " of " + picArr.length);
+			$i.fadeIn();
+		});
+		timer = setTimeout(function(){
+								displayImages(tags, index+1);
+		}, 2000);
+	};
 }
 $(document).ready(main);
